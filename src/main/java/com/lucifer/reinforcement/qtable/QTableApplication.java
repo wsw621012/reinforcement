459435c2fc7e971610d7a771c4bc88eda68b0461
Siwei Wang <siwei_wang@trend.com.tw>
2018-03-26 15:48:53 +0800

    Initialize

---
+++ src/main/java/com/lucifer/reinforcement/qtable/QTableApplication.java
@@ -0,0 +1,12 @@
+package com.lucifer.reinforcement.qtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
